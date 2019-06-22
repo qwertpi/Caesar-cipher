0d30ba45a0fc3f284e760b135351b1febf069a8c
qwertpi <qwertpi@users.noreply.github.com>
2019-06-22 11:05:53 +0100

    Added auto decrypt using bigrams and trigrams

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ from random import randint
 #regex that matches characters that are not lower case a-z and not upper case A-Z
 regex = re.compile('[^a-zA-Z]')
 
+#takes a list and returns a list sorted by the number of times the elements occured in the original list
+def SortList(l):
+    #creates a dictionary with the keys of the list elements and values of how often the key appears in the list
+    l = { x: l.count(x) for x in set(l) }
+    #sorts the dict by the keys and pulls out the keys
+    l = [x for x in sorted(l, key=l.get, reverse=True)]
+    return l
+def BigramFinder(text, n=2):
+    grams = []
+    i = 0
+    step = n-1
+    text_length = len(text)
+
+    while i+step < text_length:
+        ngram = text[i]
+        for step in range(1,n):
+            ngram+=text[i+step]
+        grams.append(ngram)
+        i+=1
+
+    return grams
+
 def encrypt(string,shift):
     #inits the variable storing our ciphertext so that we can append to it
     ciphertext=""
@@ -26,7 +48,7 @@ def encrypt(string,shift):
     return ciphertext
 
 def decrypt(string,shift=""):
-    #if we don't know the string
+    #if we don't know the shift
     if shift=="":
         #inits the variable storing our plaintext so that we can append to it
         plaintext=""
@@ -71,11 +93,21 @@ if mode=="2" or mode=="decrypt":
         plaintext=decrypt(text,shift)
         print(plaintext)
     except ValueError:
-        crib=input("Enter a crib to search for in the posible decryptions    ").lower()
+        crib=input("Enter a crib to search for in the posible decryptions if you know it else leave this blank    ").lower()
+
         plaintexts=decrypt(text)
-        while True:
+        if crib != "":
+            while True:
+                for el in plaintexts:
+                    if crib in el:
+                        print(abs(ord(el[1])-ord(text[1])))
+                        input(el)
+        else:
             for el in plaintexts:
-                if crib in el:
+                bigrams = SortList(BigramFinder(el))
+                trigrams = SortList(BigramFinder(el, 3))
+                #checks if any of the top three bigrams for enlish text are in the top two bigrams for this plaintext then does the same for the trigrams
+                if bigrams[0]=="th" or bigrams[1]=="th" or bigrams[0]=="he" or bigrams[1]=="he" or bigrams[0]=="in" or bigrams[1]=="in" or trigrams[0]=="the" or trigrams[1]=="the" or trigrams[0]=="and" or trigrams[1]=="and" or trigrams[0]=="ing" or trigrams[1]=="ing":
                     print(abs(ord(el[1])-ord(text[1])))
                     input(el)
 elif mode=="1" or mode=="encrypt":
